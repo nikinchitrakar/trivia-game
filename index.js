acbd92e8f0ae11d2e2714b79d0b6b6da58f10f4b
rojermaharjan <rogermaharjan@gmail.com>
2023-02-01 22:16:38 +0545

    Session added

--- index.js
+++ index.js
@@ -1,46 +1,87 @@
 const express = require('express');
+const sessions = require('express-session');
+const cookieParser = require("cookie-parser");
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+// App
+// creating 24 hours from milliseconds
+const oneDay = 1000 * 60 * 60 * 24;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(sessions({
+  secret: "108",
+  saveUninitialized: true,
+  resave: false,
+  cookie: { maxAge: oneDay }
+}));
 
+// Entities
 const players = [];
 const messages = [];
 
-app.get('/', (req, res) => {
+// Functions
+const getPlayer = (req) => {
+  let player = players.find(p => p.sessionId === req.session.id);
+  if (!player) {
+    player = {
+      "sessionId": req.session.id,
+      "socketId": null,
+      "name": "",
+    };
+    players.push(player);
+  }
+
+  return player;
+}
+
+// API
+app.get('/', (_, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/change-name', (req, res) => {
+app.post('/player', (req, res) => {
+  const player = getPlayer(req);
   const name = req.body.name;
   const socketId = req.body.socketId;
-  const player = players.find(p => p.socketId === socketId);
-  if (name && player) {
+  const errors = [];
+
+  if (socketId) {
+    player.socketId = socketId;
+  }
+
+  if (name) {
     const exists = players.find(p => p.name.toLowerCase() === name.toLowerCase());
 
     if (exists) {
-      res.status(400).send({'message': 'Name already exists'});
+      errors.push({
+        'name': 'Must be unique'
+      });
     } else {
       player.name = name;
-      res.send({'message': 'Name changed'});
     }
+  }
+
+  if (errors.length > 0) {
+    res.status(400).send({ 'message': 'Invalid inputs', 'errors': errors });
+  } else {
+    res.send({ 'message': 'Player updated', player });
   }
 });
 
-app.get('/messages', (req, res) => {
-  res.send(messages);
+app.get('/player', (req, res) => {
+  const player = getPlayer(req);
+  res.send({ player, messages });
 });
 
+// Socket
 io.on('connection', (socket) => {
   console.log('connected', socket.id);
-  players.push({
-    "socketId": socket.id,
-    "name": "",
-  });
 
   socket.on('disconnect', () => {
     console.log('disconnected', socket.id);
@@ -51,13 +92,9 @@ io.on('connection', (socket) => {
     messages.push(message);
     io.emit('message', message);
   });
-
-  socket.on('nameChange', (data) => {
-    const name = data.name;
-
-  });
 });
 
+// Server
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
